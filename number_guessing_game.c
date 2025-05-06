b213d981ff9e7a68a7d1752cd74e209962cfc809
Kotone Kanno <160243360+kotonekanno@users.noreply.github.com>
2025-05-06 10:04:35 +0900

    Update number_guessing_game.c
    トランプの数字を当てるゲームに変更

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -4,33 +4,54 @@
 
 int main(void)
 {
-    int ANSWER, num=0,continue_game=1;
+    int ANSWER,num=0,continue_game=1,i;
     
 	srand(time(NULL));
 	
 	while(continue_game)
 	{
-		printf("\n===　数当てゲーム　===\n");
-		ANSWER=rand()%6 + 1;
-
+		printf("\n♢♢　トランプ数当て　♢♢\n");
+        printf("数を当ててね♥　チャンスは 5回 まで♠\n");
+		
+		srand(time(NULL));
+		ANSWER=rand() % 13 + 1;
+		i=1;
+		
 		while(1)
 		{
-			printf("\n整数を入力してください：");
+			printf("\n==　%d回目のチャレンジ　==\n",i);
+			printf("1 ～ 13 のどれでしょう？：");
 			scanf("%d",&num);
-			
+
 			if(num==ANSWER){
-				printf("正解！答えは%d！\n",num);
+				printf("あたり♥　%d回目で正解♣\n",i);
 				break;
 			}
-			else if(num<ANSWER) printf("はずれ。それより大きいよ\n");
-			else printf("はずれ。それより小さいよ\n");
+			
+			else if(num<ANSWER){
+				printf("はずれ♦　それより大きいよ♠\n");
+				i++;
+				if(i>=6){
+					printf("\n残念♠　5回で当てられなかったのでゲームオーバー♣\n");
+					printf("答えは%dでした♦\n",ANSWER);
+					break;
+				}
+				else continue;
+			}
+			else{
+				printf("はずれ♦　それより小さいよ♣\n");
+				i++;
+				if(i>=6){
+					printf("\n残念♠　5回で当てられなかったのでゲームオーバー♣\n");
+					printf("答えは%dでした♦\n",ANSWER);
+					break;
+				}
+				else continue;
+			}
 		}
-		
-		printf("\nゲームを続けますか？（つづける:1　終了:0）：");
-		scanf("%d",&continue_game);
+	printf("\nゲームを続ける？（つづける:1　終了:0）：");
+	scanf("%d",&continue_game);
 	}
-    
-    printf("\nゲーム終了\n");
-     
+    printf("\nゲーム終了♣\n");
     return 0;
 }
