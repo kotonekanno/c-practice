9b853c84164f90e1b9d6c95399e1b0d7cd313ef0
Kotone Kanno <160243360+kotonekanno@users.noreply.github.com>
2025-05-15 10:36:32 +0900

    Update number_guessing_game.c
    難易度ごとにゲームを選べるように変更

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -4,54 +4,163 @@
 
 int main(void)
 {
-    int ANSWER,num=0,continue_game=1,i;
+    int ANSWER,num=0,continue_game=1,level,i;
     
 	srand(time(NULL));
 	
-	while(continue_game)
-	{
-		printf("\n♢♢　トランプ数当て　♢♢\n");
-        printf("数を当ててね♥　チャンスは 5回 まで♠\n");
-		
-		srand(time(NULL));
-		ANSWER=rand() % 13 + 1;
-		i=1;
-		
-		while(1)
-		{
-			printf("\n==　%d回目のチャレンジ　==\n",i);
-			printf("1 ～ 13 のどれでしょう？：");
-			scanf("%d",&num);
+    printf("\n◆◆ 数当てゲーム ◆◆");
+    printf("\n１：初級 サイコロ数当て");
+    printf("\n２：中級 トランプ数当て");
+    printf("\n３：上級 100までの数当て");
+    printf("\nレベルを選んでね♠：");
+    scanf("%d",&level);
+    
+    switch(level){
+		case 1:
+			while(continue_game)
+			{
+				printf("\n\n♢♢初級　サイコロ数当て　♢♢\n");
+				printf("数を当ててね♥　チャンスは 3回 まで♠\n");
 
-			if(num==ANSWER){
-				printf("あたり♥　%d回目で正解♣\n",i);
-				break;
+				srand(time(NULL));
+				ANSWER=rand() % 6 + 1;
+				i=1;
+				
+				while(1)
+				{
+					printf("\n==　%d回目のチャレンジ　==\n",i);
+					printf("1 ～ 6 のどれでしょう？：");
+					scanf("%d",&num);
+					
+					if(num==ANSWER){
+						printf("あたり♥　%d回目で正解♣\n",i);
+						break;
+					}
+					
+					else if(num<ANSWER){
+						printf("はずれ♦　それより大きいよ♠\n");
+						i++;
+						if(i>=4){
+							printf("\n残念♠　3回で当てられなかったのでゲームオーバー♣\n");
+							printf("答えは%dでした♦\n",ANSWER);
+							break;
+						}
+						else continue;
+					}
+					else{
+						printf("はずれ♦　それより小さいよ♣\n");
+						i++;
+						if(i>=4){
+							printf("\n残念♠　3回で当てられなかったのでゲームオーバー♣\n");
+							printf("答えは%dでした♦\n",ANSWER);
+							break;
+						}
+						else continue;
+					}
+				}
+			printf("\nゲームを続ける？（つづける:1　終了:0）：");
+			scanf("%d",&continue_game);
 			}
+			break;
 			
-			else if(num<ANSWER){
-				printf("はずれ♦　それより大きいよ♠\n");
-				i++;
-				if(i>=6){
-					printf("\n残念♠　5回で当てられなかったのでゲームオーバー♣\n");
-					printf("答えは%dでした♦\n",ANSWER);
-					break;
+		case 2:
+			while(continue_game)
+			{
+				printf("\n\n♢♢中級　トランプ数当て　♢♢\n");
+				printf("数を当ててね♥　チャンスは 5回 まで♠\n");
+
+				srand(time(NULL));
+				ANSWER=rand() % 13 + 1;
+				i=1;
+				
+				while(1)
+				{
+					printf("\n==　%d回目のチャレンジ　==\n",i);
+					printf("1 ～ 13 のどれでしょう？：");
+					scanf("%d",&num);
+					
+					if(num==ANSWER){
+						printf("あたり♥　%d回目で正解♣\n",i);
+						break;
+					}
+					
+					else if(num<ANSWER){
+						printf("はずれ♦　それより大きいよ♠\n");
+						i++;
+						if(i>=6){
+							printf("\n残念♠　5回で当てられなかったのでゲームオーバー♣\n");
+							printf("答えは%dでした♦\n",ANSWER);
+							break;
+						}
+						else continue;
+					}
+					else{
+						printf("はずれ♦　それより小さいよ♣\n");
+						i++;
+						if(i>=6){
+							printf("\n残念♠　5回で当てられなかったのでゲームオーバー♣\n");
+							printf("答えは%dでした♦\n",ANSWER);
+							break;
+						}
+						else continue;
+					}
 				}
-				else continue;
+			printf("\nゲームを続ける？（つづける:1　終了:0）：");
+			scanf("%d",&continue_game);
 			}
-			else{
-				printf("はずれ♦　それより小さいよ♣\n");
-				i++;
-				if(i>=6){
-					printf("\n残念♠　5回で当てられなかったのでゲームオーバー♣\n");
-					printf("答えは%dでした♦\n",ANSWER);
-					break;
+            break;
+			
+        case 3:
+			while(continue_game)
+			{
+				printf("\n\n♢♢上級　100までの数当て　♢♢\n");
+				printf("数を当ててね♥　チャンスは 7回 まで♠\n");
+
+				srand(time(NULL));
+				ANSWER=rand() % 100 + 1;
+				i=1;
+				
+				while(1)
+				{
+					printf("\n==　%d回目のチャレンジ　==\n",i);
+					printf("1 ～ 100 のどれでしょう？：");
+					scanf("%d",&num);
+					
+					if(num==ANSWER){
+						printf("あたり♥　%d回目で正解♣\n",i);
+						break;
+					}
+					
+					else if(num<ANSWER){
+						printf("はずれ♦　それより大きいよ♠\n");
+						i++;
+						if(i>=8){
+							printf("\n残念♠　7回で当てられなかったのでゲームオーバー♣\n");
+							printf("答えは%dでした♦\n",ANSWER);
+							break;
+						}
+						else continue;
+					}
+					else{
+						printf("はずれ♦　それより小さいよ♣\n");
+						i++;
+						if(i>=8){
+							printf("\n残念♠　7回で当てられなかったのでゲームオーバー♣\n");
+							printf("答えは%dでした♦\n",ANSWER);
+							break;
+						}
+						else continue;
+					}
 				}
-				else continue;
+			printf("\nゲームを続ける？（つづける:1　終了:0）：");
+			scanf("%d",&continue_game);
 			}
-		}
-	printf("\nゲームを続ける？（つづける:1　終了:0）：");
-	scanf("%d",&continue_game);
-	}
-    printf("\nゲーム終了♣\n");
-    return 0;
+            break;
+			
+        default:
+			break;
+    }
+	
+	printf("\nゲーム終了♣\n");
+	return 0;
 }
